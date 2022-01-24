7d71d638c4a7b911a07a4b549051a970354e8d0e
Rithwik Babu <rithwikbabu2020@gmail.com>
2022-01-23 23:55:45 -0600

    Updated Output dataset

--- create_dataset.py
+++ create_dataset.py
@@ -13,6 +13,11 @@ newlen = round(n/10000)*10000
 rounded_array = x[:newlen]
 
 # Make Dataset with Sliding Window
-dataset = []
+dataset_x = []
 for i in range(0,newlen,5000):
-  dataset.append(rounded_array[i:i+100000])
+  dataset_x.append(rounded_array[i:i+10001])
+
+# Split dataset into values and predicted values
+dataset = []
+for val in dataset_x:
+  dataset.append([val[:10000], val[10000:]])
